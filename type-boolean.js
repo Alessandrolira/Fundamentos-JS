eccef9e88278254b89c10c1a89f4968df6f382b4
Alessandro Lira <Alessandro.Lira@uniforja.com.br>
2022-10-10 14:05:47 -0300

    alteração de valor

--- type-boolean.js
+++ type-boolean.js
@@ -1,10 +1,10 @@
 const primeiroNumero = 5;
-const segundaNumero = 10;
+const segundaNumero = 5;
 let cadastroAtivado = true;
 
 // console.log(primeiroNumero === segundaNumero);
 
 const texto1 = "Alura";
-const texto2 = "Alura";
+const texto2 = "alura";
 
 console.log(texto1 === texto2);
