b01bca7f194c68c4c25289cc6d937714fa3a6aee
Alessandro Lira <Alessandro.Lira@uniforja.com.br>
2022-10-10 13:50:34 -0300

    fundamentos JS

---
+++ type-boolean.js
@@ -0,0 +1,10 @@
+const primeiroNumero = 5;
+const segundaNumero = 5;
+let cadastroAtivado = true;
+
+// console.log(primeiroNumero === segundaNumero);
+
+const texto1 = "Alura";
+const texto2 = "Alura";
+
+console.log(texto1 === texto2);
