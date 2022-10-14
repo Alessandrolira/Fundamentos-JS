6c5d44a1e670bc250f5692434c3133fb36155fda
Alessandro Lira <Alessandro.Lira@uniforja.com.br>
2022-10-14 07:53:28 -0300

    String(), Number()

---
+++ conversores.js
@@ -0,0 +1,15 @@
+//tipo de dado
+//booleanos
+
+
+//conversão implícita
+const numero = 456;
+const numeroString = "456";
+
+//Number()
+console.log(numero + Number(numeroString));
+//String()
+console.log(String(numero) + numeroString);
+
+
+//conversão explícita
