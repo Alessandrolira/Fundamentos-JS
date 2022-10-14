f6b3ed9d06cdd5cdc72398fc7647651ea4ee8a5c
Alessandro Lira <Alessandro.Lira@uniforja.com.br>
2022-10-14 10:48:56 -0300

    erros

---
+++ erros.js
@@ -0,0 +1,18 @@
+// const numero;
+  //SyntaxError
+  //Missing initializer in const declaration
+  const numero = 1;
+
+
+//console.log(numero;
+  //SyntaxError
+  //After argument list
+  console.log(numero);
+
+//console.log(minhaVar);
+  //ReferenceError
+  //minhaVar is not defined
+  //const minhaVar = "oi";
+  console.log(minhaVar);
+
+//StackTrace
