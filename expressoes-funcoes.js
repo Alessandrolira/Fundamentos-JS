be22cd38be81436981e52f8102f93a55a88aab7b
Alessandro Lira <Alessandro.Lira@uniforja.com.br>
2022-10-26 11:52:20 -0300

    expressões de funções

---
+++ expressoes-funcoes.js
@@ -0,0 +1,14 @@
+//expressao de função
+
+const soma = function(num1, num2) {return num1 + num2}
+// console.log(soma(1, 1));
+
+
+// diferença principal ??????
+// função e var são "listadas" no topo do arquivo.
+
+console.log(apresentar());
+
+function apresentar() {
+  return "olá";
+}
